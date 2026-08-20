1b6c173d59ed618b9f7139962ab0c8f487d9c31e
SG <151684088+LuchaLibreAAA@users.noreply.github.com>
2026-08-21 03:02:14 +0530

    Set default dark mode to true
    Change default dark mode state to true.

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import LoadingBar from 'react-top-loading-bar';
 
 
 function App() {
-  let [darkmode,setDarkMode]=useState(false)
+  let [darkmode,setDarkMode]=useState(true)
   //default color
          document.body.style.backgroundColor ='rgb(0, 82, 170)';
 
